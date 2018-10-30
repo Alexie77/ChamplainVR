cb7d2b79bd9ed085f5b6932901b30757b0b015f0
Alexie77 <mortazavi.elham@gmail.com>
2018-10-30 11:34:28 -0400

    Testing GIT Changes

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,2 +1,26 @@
 print("Hello world")
 print("abc")
+
+print("x","y")
+
+t = "test" \
+    + "a line of text"
+
+'this is a comment'
+
+'''
+this is 
+a multiline comment
+'''
+
+a = "they're"
+b = 'they\'re'
+
+fname = "elham"
+lname = "mortazavi"
+number = 7
+
+name = fname + " " + lname
+phrase = str.upper(name) + "; my favorite " \
+"number is " + str(number)
+print(phrase)
