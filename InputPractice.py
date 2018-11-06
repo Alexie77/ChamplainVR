466c3426ecb1248dba8a2071fc472c24495f7d8d
alexie77 <mortazavi.elham@gmail.com>
2018-11-06 11:26:03 -0500

    New exercise

--- InputPractice.py
+++ InputPractice.py
@@ -5,11 +5,16 @@
 x = input("Enter a number >")
 y = input("Enter a second number >")
 
+#don't use try except for everything, use it when you expect to get an exception
 try:
     result = (int(x)/int(y))
     #print(int(x)/int(y))
-except ZeroDivisionError:
+except ZeroDivisionError: #specialized exception, must always come before the general one
     print("Sorry you cannot divide by zero")
+except ValueError:
+    print("Please use vakues 0 - 9 (Except as a divisor!)")
+except Exception:   #general exception
+    print("An unknown error has occurred")
 else: #runs wheather the answer is OK or not
     #print(int(x)/int(y))
     print(result)
